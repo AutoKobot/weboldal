d8123d6475b2229bfe5fc152c638f1a6ddaf93d7
borgai74-create <borgai74@gmail.com>
2026-05-05 12:35:15 +0200

    Mentes: 2026. 05. 05. 12:35:15,23

--- FELTOLTES_TESZT_OLDALRA.bat
+++ FELTOLTES_TESZT_OLDALRA.bat
@@ -2,12 +2,8 @@
 echo [+] MENTES ES FELTOLTES A TESZT OLDALRA...
 git add .
 git commit -m "Mentes: %date% %time%"
-<<<<<<< HEAD
 echo [+] Kenyszeritett feltoltes a dev agra...
 git push origin HEAD:dev --force
-=======
-git push origin dev
->>>>>>> dev
 echo.
 echo [OK] A Teszt oldal frissitese elindult a Renderen!
 timeout /t 5
